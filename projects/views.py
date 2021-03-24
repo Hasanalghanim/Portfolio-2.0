d34112234150ab9bdeb802b60a71131bb304b454
hasan <hasanalghanim@gmail.com>
2021-03-24 00:23:16 -0600

    Final
    Final

--- projects/views.py
+++ projects/views.py
@@ -13,24 +13,7 @@ from portfolio.settings import EMAIL_HOST_USER
 
 def home(request):
     project = Project.objects.all()[:3]
-    # if request.method == 'POST':
-    #         name = request.POST['sender-name']
-    #         from_email = request.POST['from-email']
-    #         message = request.POST['from-message']
-    #         message_sent = 'Your message was sent'
-    #         sent = "Thank you. I got you Email"
-    #         error = "Please check the form"
-    #         missing_email = " "
-    #
-    #         if from_email == missing_email:
-    #             return render(request, 'projects/home.html', {"error":error}, {'project': project})
-    #         else:
-    #
-    #             try:
-    #                 send_mail([from_email, name], message, from_email, [EMAIL_HOST_USER])
-    #             except BadHeaderError:
-    #                 return HttpResponse('Invalid header found.')
-    #         return render(request, 'projects/home.html',   {'project': project},)
+
     return render(request, 'projects/home.html', {'project': project})
 
 
